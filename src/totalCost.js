d3eb728c6db949ac012951a45e972978a2b69e3b
ValentinNM <57604614+ValentinNM@users.noreply.github.com>
2021-11-02 18:08:06 +0200

    CREATE: source code for the test models

--- src/totalCost.js
+++ src/totalCost.js
@@ -1,6 +1,12 @@
-function totalCost() { 
+function totalCost(basket) { 
 
-    console.log()
+    let total = 0
+    const cart = basket.forEach(item => {
+        total += item.price * item.quantity        
+    });
+
+    return total
+    // console.log()
 }
 
 module.exports = totalCost
